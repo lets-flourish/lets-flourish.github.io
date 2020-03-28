11b2263cb6d1ee4ba73c94a6d74fc303594f8157
Jeshua Maxey <me@jeshua.co>
2020-03-28 16:42:03 +0000

    update filters

--- js/list.js
+++ js/list.js
@@ -17,31 +17,42 @@ const init = () => {
   const initialFilter = {
     area: "[data-area]",
     type: "[data-type]",
-    cta: "[data-cta]"
+    cta: "",
+    category: ""
   }
   let currentFilter = { ...initialFilter };
   
   function applyFilter(f) {
-    const filter = `${f.type}${f.area}${f.cta}`;
+    const filter = `${f.type}${f.area}${f.cta}${f.category}`;
     console.log('applying: ', filter)
     $grid.isotope({ filter });
   }
 
-  
+  // city-region-select.html
   $('select#area-filter').on('change', ev => {
     currentFilter.area = ev.target.value;
     applyFilter(currentFilter);
   })
 
+  // cta-select.html
   $('select#cta-filter').on('change', ev => {
     currentFilter.cta = ev.target.value;
     applyFilter(currentFilter);
   })
   
+  // og filter button array
   $('.nav-item > .btn.btn-flourish').on('click', ev => {
     currentFilter.type = ev.target.getAttribute('filter-by');
     applyFilter(currentFilter);
     $('button[filter-by]').removeClass('active');
+    $(ev.target).addClass('active');
+  });
+
+  // filter-button-array.html
+  $('.btn.btn-flourish.filter').on('click', ev => {
+    currentFilter.category = ev.target.getAttribute('data-filter-by');
+    applyFilter(currentFilter);
+    $('button[data-filter-by]').removeClass('active');
     $(ev.target).addClass('active');
   });
 
